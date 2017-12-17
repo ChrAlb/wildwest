3ac852eb96de75d876d46f04ea0773611b5e32d2
ChrAlb <albchr@gmail.com>
2017-12-17 14:04:31 +0100

    Working on EnemyClass

--- WildWest/Enemy.cpp
+++ WildWest/Enemy.cpp
@@ -15,17 +15,38 @@ Enemy::Enemy() : m_hasDestination(false)
 
 bool Enemy::handleInput(float dt) 
 {
-	//PlayableCharacter::update(dt);
 	
-	if (m_hasDestination)
-	{
-		//if (abs(m_destination.x - m_Po))  Hier weiter bauen.....
-	}
-	return true;
 
 }
 
 void Enemy::update(float dt)
 {
-
+    Enemy::update(dt);
+	
+	if (m_hasDestination)
+	{
+		if (abs(m_destination.x - m_Position.x) < 16)
+		{
+			m_hasDestination = false;
+			return;
+		}
+		if (m_destination.x - m_Position.x > 0)
+		{
+			m_Position.x += m_Speed * dt;
+		}
+		else
+		{
+			m_Position.x -= m_Speed * dt;
+		}
+		//Hier Collisionstest?
+		int random = rand() % 1000 + 1;
+		if (random != 1000) { return; }
+		int newX = rand() % 65 + 0;
+		if (rand() % 2) {newX = -newX; }
+		m_destination.x = m_Position.x + newX;
+		if (m_destination.x < 0) { m_destination.x = 0; }
+		m_hasDestination = true;
+		 
+	}
+	
 }
