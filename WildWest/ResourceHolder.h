2f7ea654f72069537fe831b48b619192bbc6ddeb
ChrAlb <albchr@gmail.com>
2018-07-06 21:44:17 +0200

    tt

--- WildWest/ResourceHolder.h
+++ WildWest/ResourceHolder.h
@@ -11,7 +11,7 @@
 template <typename Resource, typename Identifier>
 class ResourceHolder
 {
-	public:
+	public: 
 		void						load(Identifier id, const std::string& filename);
 
 		template <typename Parameter>
